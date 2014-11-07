7a96cfb5a43e80afcd5e24d2ebf26df4fde97f28
Aoxi Li <aoxili@hudson-trading.com>
2014-11-07 13:00:32 -0500

    ln existing files

--- link.sh
+++ link.sh
@@ -1,7 +1,7 @@
 git clone https://github.com/Lokaltog/powerline-fonts ~/.powerline-fonts
 ~/.powerline-fonts/install.sh
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/.dotfiles/vimrc ~/.vimrc
-ln -s ~/.dotfiles/wakatime.cfg ~/.wakatime.cfg
+ln -nfs ~/.dotfiles/git/gitconfig ~/.gitconfig
+ln -nfs ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -nfs ~/.dotfiles/vimrc ~/.vimrc
+ln -nfs ~/.dotfiles/wakatime.cfg ~/.wakatime.cfg
