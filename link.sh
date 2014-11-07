24373622479cc376582e72b31c3d07eee761f6be
Aoxi Li <aoxili@hudson-trading.com>
2014-11-07 10:04:22 -0500

    add wakatime api and vundle powerline-fonts installation

--- link.sh
+++ link.sh
@@ -1,3 +1,7 @@
-ln -s ~/dotfiles/git/gitconfig ~/.gitconfig
-ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/vimrc ~/.vimrc
+git clone https://github.com/Lokaltog/powerline-fonts ~/.powerline-fonts
+~/.powerline-fonts/install.sh
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/vimrc ~/.vimrc
+ln -s ~/.dotfiles/wakatime.cfg ~/.wakatime.cfg
