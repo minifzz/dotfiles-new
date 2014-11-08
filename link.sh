3e82ccab86151d3b15f9065191f8e20be6dc9b5e
Aoxi Li <eray.aoxili@gmail.com>
2014-11-07 22:09:55 -0500

    link.sh runs on demand. move powerline fonts in as submodule.

--- link.sh
+++ link.sh
@@ -1,6 +1,22 @@
-git clone https://github.com/Lokaltog/powerline-fonts ~/.powerline-fonts
-~/.powerline-fonts/install.sh
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [[ `uname` == 'Darwin' ]]; then
+    # MacOS
+    font_dir="$HOME/Library/Fonts"
+else
+    # Linux
+    font_dir="$HOME/.fonts"
+fi
+
+[[ ! -d "$font_dir" || -z "$(ls $font_dir | grep -i powerline)" ]] && \
+  echo "Installing Powerline Font" && ~/.dotfiles/powerline-fonts/install.sh
+
+vundle="$HOME/.vim/bundle/Vundle.vim"
+[[ ! -d $vundle ]] && echo "download vundle" && \
+  git clone https://github.com/gmarik/Vundle.vim.git $vundle
+
+oh_my_zsh="$HOME/.oh-my-zsh"
+[[ ! -d $oh_my_zsh ]] && echo "download oh-my-zsh" && \
+  (curl -L http://install.ohmyz.sh | sh)
+
 ln -nfs ~/.dotfiles/git/gitconfig ~/.gitconfig
 ln -nfs ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -nfs ~/.dotfiles/vimrc ~/.vimrc
